eb20283c0075645dc2309347f4a548c14558f170
liu <821@qq.com>
2023-02-13 20:44:48 +0800

    注册功能

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from utils.converters import UsernameConverter, MobileConverter
+from utils.converters import UsernameConverter, MobileConverter, UUIDConverter
 from django.urls import register_converter
 
 # 注册转换器
 register_converter(UsernameConverter, 'username')
 register_converter(MobileConverter, 'mobile')
+register_converter(UUIDConverter,'uuid')
 
 urlpatterns = [
     path('', include('apps.users.urls')),
